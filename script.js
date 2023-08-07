700f5cf998bb7f23eb88813823a551c5ed822c64
KTC96 <kyleclow96@gmail.com>
2023-08-07 19:39:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let toggleMenu = document.getElementById("hamburgerMenu");
+/**
+ * Hamburgermenu toggle to open and close
+ */
+function openMenu() {
+    toggleMenu.style.display = "block";
+}
+
+function closeMenu() {
+    toggleMenu.style.display = "none";
+}
